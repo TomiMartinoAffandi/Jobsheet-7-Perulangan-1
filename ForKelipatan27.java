397bcecb12560890e892260b1ad4c34da4ab176e
TomiMartinoAffandi <tomimartino10@gmail.com>
2023-10-12 21:36:42 +0700

    modify 2

--- ForKelipatan27.java
+++ ForKelipatan27.java
@@ -7,6 +7,7 @@ public class ForKelipatan27 {
         int kelipatan, 
         jumlah = 0, 
         counter = 0;
+        float rataRata = 0;
 
         System.out.println("masukan bilangan kelipatan (1-9) : ");
         kelipatan = scan.nextInt();
@@ -15,13 +16,14 @@ public class ForKelipatan27 {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
-                
             }
             
         }
+        rataRata = jumlah / counter;
 
         System.out.printf("banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("total bilangan kelipatan %d dari 1 sampai 50 adlah %d\n", kelipatan, jumlah);
+        System.out.printf("rata-rata dari %d adalah %f\n", kelipatan, rataRata);
 
     }
 }
