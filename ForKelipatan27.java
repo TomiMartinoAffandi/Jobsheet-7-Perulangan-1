69afb144c50dd1a4d14c810e775a85ced0922a6c
TomiMartinoAffandi <tomimartino10@gmail.com>
2023-10-12 19:31:24 +0700

    percobaan 1

---
+++ ForKelipatan27.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ForKelipatan27 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int kelipatan, 
+        jumlah = 0, 
+        counter = 0;
+
+        System.out.println("masukan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        for (int i = 1 ; i <= 50 ; i++)  {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+                
+            }
+            
+        }
+
+        System.out.printf("banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("total bilangan kelipatan %d dari 1 sampai 50 adlah %d\n", kelipatan, jumlah);
+
+    }
+}
