ccbd4f45df7acaff6919dd25976cb430f9058665
TomiMartinoAffandi <tomimartino10@gmail.com>
2023-10-14 21:54:24 +0700

    ini project tugas pertemuan  7

---
+++ tugasp7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class tugasp7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int banyakPengiriman = 0;
+        double disc = 0, biaya = 0, biayaTotal = 0;
+        
+        do {
+            System.out.print("Berapa kali anda sudah mengirim paket di ekspedisi kami? ");
+            banyakPengiriman = sc.nextInt();
+            System.out.print("Masukkan biaya ongkos kirim yang sudah dihitung sebelumnya: ");
+            biaya = sc.nextInt();
+
+                if (banyakPengiriman <= 9) {
+                    
+                    if (banyakPengiriman % 5 == 0) {
+                    disc = 0.5;               
+                    }
+                break;
+                }
+            biayaTotal = biaya - biaya*disc;
+            System.out.println("Total biaya ongkos kirim anda adalah: " +biayaTotal );
+
+
+            System.out.print("ingin menambahkan inputan baru? (y/t)");
+            String konfirmasi = sc.next();
+                if (konfirmasi.equals("t")) {
+                    break;
+                }
+            
+        }while(banyakPengiriman >= 9);
+    }
+}
